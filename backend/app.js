d7e4adf794edf3d11318599e561fe967f6ce0317
anilkumar2564 <anilkaliya.786@gmail.com>
2020-04-26 21:01:39 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,11 @@ const postsRoutes = require("./routes/posts");
 const app = express();
 
 
-const p=path.join(__dirname, '/../build/', 'index.html')
-console.log(p);
-
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({ extended: false}));
- app.use(express.static(path.join(__dirname,'/../build/')));
+ app.use(express.static(path.join(__dirname +'/../build/')));
  app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/../build/', 'index.html'));
+  res.sendFile(path.join(__dirname+ '/./build/index.html'));
 });
 
 mongoose
